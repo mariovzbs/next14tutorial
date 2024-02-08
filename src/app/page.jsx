c27917673c091f151acb457721d555464fac6fa1
Mario V <mariovzbs@gmail.com>
2024-02-07 20:21:08 -0600

    Implemented home page design

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,38 @@
+import Image from "next/image";
+import styles from "./home.module.css";
+
 const Home = () => {
-  return <>Homepage</>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Creative Thoughrs Agency</h1>
+        <p className={styles.desc}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam magni
+          culpa corporis natus, porro cumque consequatur doloremque, laborum.
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image
+            src="/brands.png"
+            alt="brands"
+            fill
+            className={styles.brandImg}
+          />
+        </div>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image
+          src="/hero.gif"
+          alt="hero image"
+          fill
+          className={styles.heroImg}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
